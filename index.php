d276212d88d8880ed89999f05fd4155de22a220f
ruan-afri <ruan.prinsloo@afrihost.com>
2016-02-23 17:15:09 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//Connection Variables
+$servername = "localhost";
+$username = "papyri_user";
+$password = "mkv2vob2010";
+
+try {
+
+    $conn = new PDO("mysql:host=$servername;dbname=papyri_db", $username, $password);
+    // Set the PDO error mode to 'exception'
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    }
+
+catch(PDOException $e)
+    {
+
+    echo "Connection Failed: " . $e->getMessage();
+
+    }
+
+?>
